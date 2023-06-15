1b538c1492f42390ab51ebc5442ed9fcda2b625a
Harsh <goyalh4164@gmail.com>
2023-06-15 14:39:19 +0530

    product card

--- frontend/src/components/ProductsCard.jsx
+++ frontend/src/components/ProductsCard.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Box, Image, Badge, Text } from "@chakra-ui/react";
 
 function ProductsCard(props) {
+    const { title, image, description, price, category } = props;
+
     return (
-        <div>
-            
-        </div>
+        <Box
+          maxW="sm"
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+          boxShadow="lg"
+        >
+          <Image src={image} alt={title} />
+    
+          <Box p="6">
+            <Box d="flex" alignItems="baseline">
+              <Badge borderRadius="full" px="2" colorScheme="teal">
+                {category}
+              </Badge>
+            </Box>
+    
+            <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
+              {title}
+            </Box>
+    
+            <Text mt="1">{description}</Text>
+    
+            <Box d="flex" mt="2" alignItems="center">
+              <Box fontWeight="semibold" as="h4" fontSize="lg">
+                ${price}
+              </Box>
+            </Box>
+          </Box>
+        </Box>
     );
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import ProductsCard from '../components/ProductsCard';
 
 function Home(props) {
+    const product = {
+        title: "Product Title",
+        image: "..\src\assets\Website Logo\logo-black.png",
+        description: "Product description goes here",
+        price: 19.99,
+        category: "Category",
+    };
+
     return (
         <div>
             Home
+            <ProductsCard
+            title={product.title}
+            image={product.image}
+            description={product.description}
+            price={product.price}
+            category={product.category}
+        />
         </div>
     );
 }
