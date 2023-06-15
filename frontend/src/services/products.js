fccd39baa977c47738fcb5bf14b17fcc7c0dda78
Mohd Aftab <web.dev.aftab@gmail.com>
2023-06-16 00:41:33 +0530

    fetch product

---
+++ frontend/src/services/products.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export async function getAllListings() {
+  try {
+    const response = await axios.get("https://localhost:4000/api/listings");
+
+    const products = response.data;
+
+    return products;
+  } catch (error) {
+    console.error(error);
+    throw "Product does not exist";
+  }
+}
+
+export async function getProductById(productId) {
+  try {
+    const response = await axios.get(
+      `https://localhost:4000/api/listings${productId}`
+    );
+
+    const product = response.data;
+
+    return product;
+  } catch (error) {
+    console.error(error);
+    // Handle the error appropriately
+  }
+}
