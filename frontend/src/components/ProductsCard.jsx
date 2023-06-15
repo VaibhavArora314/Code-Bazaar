8a76d90ab35f627613a97012a80af4a47a58a2e6
Mohd Aftab <web.dev.aftab@gmail.com>
2023-06-15 16:08:37 +0530

    product

--- frontend/src/components/ProductsCard.jsx
+++ frontend/src/components/ProductsCard.jsx
@@ -1,40 +1,54 @@
-import React from 'react';
+import React from "react";
 import { Box, Image, Badge, Text } from "@chakra-ui/react";
+import DemoProduct from "../assets/Products/Demo.jpg";
 
 function ProductsCard(props) {
-    const { title, image, description, price, category } = props;
+  const { title, image, description, price, category } = props;
 
-    return (
+  return (
+    <Box
+      maxW="sm"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      boxShadow="lg"
+    >
+      <Image
+        src={image}
+        alt={title}
+        fallbackSrc={DemoProduct}
+        w="100"
+        h="100"
+        fit="cover"
+      />
+
+      <Box p="6">
+        <Box d="flex" alignItems="baseline">
+          <Badge borderRadius="full" px="2" colorScheme="teal">
+            {category}
+          </Badge>
+        </Box>
+
         <Box
-          maxW="sm"
-          borderWidth="1px"
-          borderRadius="lg"
-          overflow="hidden"
-          boxShadow="lg"
+          mt="1"
+          fontWeight="semibold"
+          as="h4"
+          lineHeight="tight"
+          isTruncated
         >
-          <Image src={image} alt={title} />
-    
-          <Box p="6">
-            <Box d="flex" alignItems="baseline">
-              <Badge borderRadius="full" px="2" colorScheme="teal">
-                {category}
-              </Badge>
-            </Box>
-    
-            <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
-              {title}
-            </Box>
-    
-            <Text mt="1">{description}</Text>
-    
-            <Box d="flex" mt="2" alignItems="center">
-              <Box fontWeight="semibold" as="h4" fontSize="lg">
-                ${price}
-              </Box>
-            </Box>
+          {title}
+        </Box>
+
+        <Text mt="1">{description}</Text>
+
+        <Box d="flex" mt="2" alignItems="center">
+          <Box fontWeight="semibold" as="h4" fontSize="lg">
+            ${price}
           </Box>
         </Box>
-    );
+      </Box>
+    </Box>
+  );
 }
 
 export default ProductsCard;
--- frontend/src/pages/ProductsList.jsx
+++ frontend/src/pages/ProductsList.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
+import {
+  Box,
+  Center,
+  useColorModeValue,
+  Heading,
+  Text,
+  Stack,
+  Image,
+} from "@chakra-ui/react";
+import DemoProduct from "../assets/Products/Demo.jpg";
 
-function ProductsList(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+const IMAGE = DemoProduct;
+
+export default function ProductsList() {
+  return (
+    <Center py={12}>
+      <Box
+        role={"group"}
+        p={6}
+        maxW={"330px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.800")}
+        boxShadow={"2xl"}
+        rounded={"lg"}
+        pos={"relative"}
+        zIndex={1}
+      >
+        <Box
+          rounded={"lg"}
+          mt={-12}
+          pos={"relative"}
+          height={"230px"}
+          _after={{
+            transition: "all .3s ease",
+            content: '""',
+            w: "full",
+            h: "full",
+            pos: "absolute",
+            top: 5,
+            left: 0,
+            backgroundImage: `url(${IMAGE})`,
+            filter: "blur(15px)",
+            zIndex: -1,
+          }}
+          _groupHover={{
+            _after: {
+              filter: "blur(20px)",
+            },
+          }}
+        >
+          <Image
+            rounded={"lg"}
+            height={230}
+            width={282}
+            objectFit={"cover"}
+            src={IMAGE}
+          />
+        </Box>
+        <Stack pt={10} align={"center"}>
+          <Text color={"gray.500"} fontSize={"sm"} textTransform={"uppercase"}>
+            category
+          </Text>
+          <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
+            Title
+          </Heading>
+          <Stack direction={"row"} align={"center"}>
+            <Text fontWeight={800} fontSize={"xl"}>
+              $57
+            </Text>
+            <Text textDecoration={"line-through"} color={"gray.600"}>
+              $199
+            </Text>
+          </Stack>
+        </Stack>
+      </Box>
+    </Center>
+  );
 }
-
-export default ProductsList;
