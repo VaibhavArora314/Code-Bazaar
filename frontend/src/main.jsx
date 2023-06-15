c4b6e39f85e69f4d8a3c3ed62905997d034ac247
Vaibhav Arora <vaibhavarora0301vishu@gmail.com>
2023-06-15 12:58:05 +0530

    Auth and extendTheme error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext.js";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 const config = {
   initialColorMode: "light",
